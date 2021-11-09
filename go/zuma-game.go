9f7e24f27496e91215d1282e1777f3b9091772ca
Lei, Torres | Torres | ECMPD <torres.lei@rakuten.com>
2021-11-09 15:05:06 +0900

    updates

---
+++ go/zuma-game.go
@@ -0,0 +1,7 @@
+// 488. 祖玛游戏 - Hard
+// https://leetcode-cn.com/problems/zuma-game/
+package leetcode
+
+func findMinStep(board string, hand string) int {
+	return -1
+}
---
+++ python/zuma-game.py
@@ -0,0 +1,5 @@
+# 488. 祖玛游戏
+# https://leetcode-cn.com/problems/zuma-game/
+class Solution:
+    def findMinStep(self, board: str, hand: str) -> int:
+        return -1
