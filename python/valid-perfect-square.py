762f971c2a117c51596f25f7314427de0477b649
torres.lei <torres.lei@rakuten.com>
2021-11-17 11:11:26 +0900

    updates

--- python/valid-perfect-square.py
+++ python/valid-perfect-square.py
@@ -2,6 +2,10 @@
 # https://leetcode-cn.com/problems/valid-perfect-square/
 
 def isPerfectSquare(num: int) -> bool:
+    """
+    If a number is perfect square number, then it can be subtracted by 1, 3, 5, 7...
+    Util the value of the number is 0
+    """
     sub = 1
     while num > 0:
         num -= sub
